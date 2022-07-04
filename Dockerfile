d5de520243e9270d2fb50c3939486b9c716ffca6
ushigai <usigaifumito@gmail.com>
2022-07-04 13:58:20 +0900

    ﾄﾞｯｶｰﾄﾞｶﾄﾞｶ

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:latest
+
+WORKDIR /app
+COPY . /app
+
+RUN go mod init main \
+    && go mod tidy \
+    && go build
+
+ENV CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64
+EXPOSE 8080
+
+
+CMD ["go", "run", "main.go"]
+
--- main.go
+++ main.go
@@ -16,14 +16,6 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-type ServiceInfo struct {
-	Title string
-}
-
-var serviceInfo = ServiceInfo {
-	"サイトのタイトル",
-}
-
 func main() {
 	t := &Template{
 		templates: template.Must(template.ParseGlob("views/*.html")),
@@ -60,7 +52,7 @@ func main() {
 	})
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "こんにちは!")
+		return c.String(http.StatusOK, "Here is root :)")
 	})
 
 	//e.GET("/users/:id", func(c echo.Context) error {
@@ -72,12 +64,16 @@ func main() {
 	
 	g := e.Group("/admin")
 	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		if username == "joe" && password == "secret" {
+		if username == "joe" && password == "secret1" {
+			//data := struct {
+				//info string
+			//} {
+				//"admin",
+			//}
 			return true, nil
 		}
 		return false, nil
 	}))
-
 
     e.Logger.Fatal(e.Start(":1323"))
 }
