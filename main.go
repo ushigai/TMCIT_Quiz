eeb9418298a625863b35a27d0aa6f8e5296b84e5
ushigai <usigaifumito@gmail.com>
2022-07-11 01:59:21 +0900

    おやすみコミット💤

--- main.go
+++ main.go
@@ -102,70 +102,20 @@ func main() {
     }
 	e.Renderer = t
 
-    e.GET("/lobby", GetRoom)
-	//e.GET("/lobby", func(c echo.Context) error{
-		//// TODO: ここらへんDBと連携する
-		//data := struct {
-			//LobbyID string
-			//LobbyName string
-		//} {
-			//"123456789",
-			//"1300の歴史",
-		//}
-		//return c.Render(http.StatusOK, "lobby", data)
-	//})
 	e.GET("/login", func(c echo.Context) error{
-		data := struct {
-			info string
-		} {
-			"login",
-		}
+		data := struct { info string } { "login", }
 		return c.Render(http.StatusOK, "login", data)
 	})
 	e.GET("/home", func(c echo.Context) error{
-		data := struct {
-			info string
-		} {
-			"home",
-		}
+		data := struct { info string } { "home", }
 		return c.Render(http.StatusOK, "home", data)
 	})
-	//e.GET("/quiz", func(c echo.Context) error{
-		//data := struct {
-			//info string
-		//} {
-			//"quiz",
-		//}
-		//return c.Render(http.StatusOK, "quiz", data)
-	//})
-	e.GET("/collect", func(c echo.Context) error{
-		data := struct {
-			info string
-		} {
-			"collect",
-		}
-		return c.Render(http.StatusOK, "collect", data)
-	})
-	e.GET("/wrong", func(c echo.Context) error{
-		data := struct {
-			info string
-		} {
-			"wrong",
-		}
-		return c.Render(http.StatusOK, "wrong", data)
-	})
-	e.GET("/timeout", func(c echo.Context) error{
-		data := struct {
-			info string
-		} {
-			"timeout",
-		}
-		return c.Render(http.StatusOK, "timeout", data)
-	})
 	
-    e.GET("/quiz/:RoomID/:QuizID", StartQuiz)
     e.GET("/lobby/:QuizID", GetQuiz)
+    e.GET("/lobby", GetRoom)
     e.GET("/room/:RoomID", GetQuiz)
+    e.GET("/quiz/:RoomID/:QuizID", StartQuiz)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Here is root :)")
 	})
