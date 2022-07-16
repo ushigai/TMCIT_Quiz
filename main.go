25586838b0b4419f7182086f4167e236b1a97185
ushigai <usigaifumito@gmail.com>
2022-07-16 23:35:38 +0900

    はいゴミ

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
     "net/http"
 	"html/template"
 	"io"
-	"fmt"
-	"strconv"
-	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/go-sql-driver/mysql"
@@ -148,3 +145,37 @@ func main() {
     e.Logger.Fatal(e.Start(":8080"))
 
 }
+
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	USER := "go_test"
+	PASS := "password"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "go_database"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic("DB接続失敗")
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count > 180 {
+				fmt.Println("")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+	//db.LogMode(true)
+	return db
+}
+
