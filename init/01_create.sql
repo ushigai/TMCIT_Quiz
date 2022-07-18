3f5096b4c9150c2c6e620bc1b751eed809efd62c
ushigai <usigaifumito@gmail.com>
2022-07-19 06:09:21 +0900

    おやすみコミット💤

--- init/01_create.sql
+++ init/01_create.sql
@@ -13,7 +13,31 @@ CREATE TABLE if not exists rooms (
 	PRIMARY KEY (id)
 );
 
+CREATE TABLE if not EXISTS quizzes (
+	quiz_id INT NOT NULL AUTO_INCREMENT,
+	room_id INT,
+	statement VARCHAR(255),
+	answer VARCHAR(255),
+	choice1 VARCHAR(255),
+	choice2 VARCHAR(255),
+	choice3 VARCHAR(255),
+	choice4 VARCHAR(255)
+)
+
 INSERT INTO rooms(title, subtitle, author, date, comment) 
 			VALUES("1300", "seiji", "ushigai", "2022/07/01", "nandayona");
 INSERT INTO rooms(title, subtitle, author, date, comment) 
 			VALUES("1300", "kids", "shoma", "2022/07/02", "shoma kami");
+
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(1, "test statement 1-1", "test answer", 
+"test choice1", "test choice2", "test choice1", "test choice2");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(1, "test statement 1-2", "test answer", 
+"test choice1", "test choice2", "test choice1", "test choice2");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(2, "test statement 2-1", "test answer", 
+"test choice1", "test choice2", "test choice1", "test choice2");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(2, "test statement 2-2", "test answer", 
+"test choice1", "test choice2", "test choice1", "test choice2");
