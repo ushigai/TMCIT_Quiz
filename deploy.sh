4afcfbbfcf923cee290d81a268ee901b2509cb23
root <shoma564>
2022-07-24 09:54:23 +0900

    ke

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ sudo -u pi kubectl delete -f /home/pi/go_app/hon/TMCIT_Quiz/k8s/deploy.yml -f /h
 sleep 10
 sudo -u pi kubectl apply -f /home/pi/go_app/hon/TMCIT_Quiz/k8s/deploy.yml -f /home/pi/go_app/hon/TMCIT_Quiz/k8s/nodeport.yml
 sleep 10
-sudo -u pi kubectl get pod
-sudo -u pi kubectl get svc
+sudo -u pi kubectl get pod -n kobayakawake
+sudo -u pi kubectl get svc -n kobayakawake
