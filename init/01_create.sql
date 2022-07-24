50981413732f3ade0d272ec3f5772b8fb58c79f0
ushigai <usigaifumito@gmail.com>
2022-07-25 02:28:25 +0900

    おさかなー🐟

--- init/01_create.sql
+++ init/01_create.sql
@@ -27,18 +27,34 @@ CREATE TABLE if not EXISTS quizzes (
 
 INSERT INTO rooms(title, subtitle, author, date, comment) 
 			VALUES("5400", "海上先生", "broccoli", "2022/07/24", "後期期末試験");	
-INSERT INTO rooms(title, subtitle, author, date, comment) 
-			VALUES("5400", "黒木さん", "broccoli", "2022/07/24", "後期期末試験");	
-
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(1, "蓄電池のスペルを答えよ 3-1", "storage battery", "storge battery", "storage battery", "strage battery", "storege battery");
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(1, "発電のスペルを答えよ 3-2", "power generation", "power generate", "power generating", "power generation", "power generated");
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(1, "変電所のスペルを答えよ 3-3", "substation", "substation", "life span", "rectify", "fluctuations");
+
+INSERT INTO rooms(title, subtitle, author, date, comment) 
+			VALUES("5400", "黒木さん", "broccoli", "2022/07/24", "後期期末試験");	
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(2, "OSPFの正式名称 4-1", "Open Shortest Path First", "Open Start Protocol Fuck", "Open Short Protocol Friends", "Opening Sample Prototype Fast", "Open Shortest Path First");
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(2, "STPの正式名称 4-2", "Spanning Tree Protocol", "Spanning Tree Protocol", "Special Thanks Price", "Smart Telephone Protocol", "Step Trun Pumpkin");
 INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
 VALUES(2, "DHCPの正式名称 4-3", "Dynamic Host Configuration Protocol", "Dynamic Host Configuration Protocol", "Dance Hop Control Protocol", "Dream Hyper Clone Proctocol", "Dokidoki Hassuru Coreppoi Protocol");
+
+INSERT INTO rooms(title, subtitle, author, date, comment) 
+			VALUES("日本語検定", "5級", "えらいひと", "2022/07/24", "生まれは宇宙");	
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "朝起きたら？", "パンツ", "パン", "ごはん", "パンツ", "フライパン");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "ここはどこ？わたしはだれ？", "チャーリーとチョコレート工場", "チャーリーとチョコレート工場", "穴と雪の女王", "すもも", "諦めたらここで試合終了ですよ");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "人類は進化した", "ネッコ", "ワンコ", "ニャンコ", "ネッコ", "イッヌ");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "某で前日招集がかかったよ", "私は行かないがお前は行け", "いく", "いかない", "私は行かないがお前は行け", "私は行くがお前は来るな");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "10月の天気がいい日（気温22℃前後）で東京朝9時出発、車2時間以内で出かける場所と言えば？", "752", "宇都宮", "北海道", "ノストラダムス", "752");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
+VALUES(3, "魔人拳でガークラしたよ", "帰る", "帰る", "帰る", "帰る", "帰る");
+INSERT INTO quizzes(room_id, statement, answer, choice1, choice2, choice3, choice4)
